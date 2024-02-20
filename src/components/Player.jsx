10145451c05ce473ea9e619ecb12f4b764017e85
JuanCeja <jmc347ls2@yahoo.com>
2024-02-19 22:18:52 -0800

    create pseudo code for creating logic for button on Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const Player = ({ player, symbol }) => {
+
+    // use state - is edit button active
+
     return (
         <li>
             <span className="player-info">
+            
+            {/* if edit button is active display a field element instead of the span element */}
                 <span className="player-name">{player}</span>
                 <span className="player-symbol">{symbol}</span>
             </span>
+
+            {/* if button is active show save instead */}
             <button>Edit</button>
         </li>
     )
