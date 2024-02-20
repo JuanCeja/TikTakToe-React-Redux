e19240a1f79925c00bda139e96de2be9ddae3902
JuanCeja <jmc347ls2@yahoo.com>
2024-02-19 22:40:48 -0800

    complete exercise for dynamic rendering on input element

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Player = ({ player, symbol }) => {
 
-    // use state - is edit button active
+    const [isEditing, setIsEditing] = useState(false);
+
+    const editingField = () => {
+        setIsEditing(true);
+    }
 
     return (
         <li>
             <span className="player-info">
-            
-            {/* if edit button is active display a field element instead of the span element */}
-                <span className="player-name">{player}</span>
+                {
+                    isEditing ? <input />
+                        : <span className="player-name">{player}</span>
+                }
                 <span className="player-symbol">{symbol}</span>
             </span>
-
-            {/* if button is active show save instead */}
-            <button>Edit</button>
+            <button onClick={editingField}>Edit</button>
         </li>
     )
 }
