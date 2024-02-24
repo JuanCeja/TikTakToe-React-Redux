fed9394a41cd8e9b41e1656a5f3abc3a94a5679f
JuanCeja <jmc347ls2@yahoo.com>
2024-02-23 21:13:34 -0800

    recreate solution to re render the multi dimensional lists as our gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -9,9 +9,19 @@ const initialGameBoard = [
 const GameBoard = () => {
     return (
         <ol id='game-board'>
-            
+            {initialGameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
         </ol>
     )
-}
+};
 
 export default GameBoard;
